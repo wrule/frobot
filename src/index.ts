021774f9d97dd5bb35e5b1962eb33c26d3fa4967
jimao <jimao@jimaodeMBP.lan>
2022-05-02 22:00:16 +0800

    提交

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import ccxt from 'ccxt';
+// @ts-ignore
+import Secret from '../.secret.json';
 
-console.log('你好，世界');
+// console.log('你好，世界', Secret.API_KEY);
+console.log(ccxt.exchanges);
+const a = new ccxt.binance({
+  apiKey: Secret.API_KEY,
+  secret: Secret.SECRET_KEY,
+});
+console.log(a);
