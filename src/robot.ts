e733e90507e51ad348c385a7601ab08ea78eb999
jimao <jimao@jimaodeMBP.lan>
2022-05-03 22:50:47 +0800

    提交

---
+++ src/robot.ts
@@ -0,0 +1,52 @@
+
+export
+interface IRobot {
+  /**
+   * 交易币种
+   */
+  symbol: string;
+  /**
+   * 做单倍数
+   */
+  lever: number;
+  /**
+   * 开仓方向
+   */
+  direction: number;
+  /**
+   * 首单额度
+   */
+  first_amount: number;
+  /**
+   * 做单数量
+   */
+  order_number: number;
+  /**
+   * 止盈比例
+   */
+  pstop_ratio: number;
+  /**
+   * 止盈回调
+   */
+  pstop_pullback_ratio: number;
+  /**
+   * 补仓跌幅
+   */
+  fix_drop_ratios: number[];
+  /**
+   * 补仓回调
+   */
+  fix_pullback_ratio: number;
+  /**
+   * 补仓模式
+   */
+  fix_mode: string;
+  /**
+   * 检测K线
+   */
+  timeframe: string;
+  /**
+   * 趋势做单
+   */
+  trend_time: string;
+}
