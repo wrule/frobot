3334f50fe39570cbdfb3a324fda372865f0a1091
jimao <jimao@jimaodeMBP.lan>
2022-05-07 00:12:59 +0800

    提交

--- src/kline_watcher.ts
+++ src/kline_watcher.ts
@@ -1,8 +1,30 @@
 
 import ccxt, { Ticker } from 'ccxt';
 import fs from 'fs';
-import moment from 'moment';
+import moment, { Moment } from 'moment';
 import { ETimeFrame, TimeFrame } from './timeframe';
+
+export
+interface KLine {
+  time: Moment;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+}
+
+export
+function ArrayToKLine(array: number[]): KLine {
+  return {
+    time: moment(new Date(array[0])),
+    open: array[1],
+    high: array[2],
+    low: array[3],
+    close: array[4],
+    volume: array[5],
+  };
+}
 
 export
 class KLineWatcher {
@@ -16,6 +38,7 @@ class KLineWatcher {
 
   private timer: any = -1;
   private since?: number = undefined;
+  private klines: KLine[] = [];
 
   private async loopQuery() {
     clearTimeout(this.timer);
@@ -26,10 +49,25 @@ class KLineWatcher {
         this.since,
         1000,
       );
-      if (this.callback) {
-        this.callback(result);
-      } else {
-        console.log(result.length);
+      if (result.length > 0) {
+        if (this.klines.length > 0) {
+          result.forEach((item, index) => {
+            const kline = ArrayToKLine(item);
+            if (index > 0) {
+              this.klines.push(kline);
+            } else {
+              this.klines[this.klines.length - 1] = kline;
+            }
+          });
+        } else {
+          this.klines = result.map((item) => ArrayToKLine(item));
+        }
+        this.since = result[result.length - 1][0];
+        if (this.callback) {
+          this.callback(result);
+        } else {
+          console.log(result.length);
+        }
       }
     } catch (e) {
       console.log(e);
@@ -41,6 +79,7 @@ class KLineWatcher {
   }
 
   public Start() {
+    this.since = undefined;
     this.loopQuery();
   }
 
